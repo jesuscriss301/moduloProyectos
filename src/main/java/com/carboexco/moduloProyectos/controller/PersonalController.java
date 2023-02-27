bdbdd3c9c491fa154d0f04d4908a489327434e94
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-02-27 16:21:37 -0500

    controlador Personal
    controlador basico de personal

---
+++ src/main/java/com/carboexco/moduloProyectos/controller/PersonalController.java
@@ -0,0 +1,73 @@
+package com.carboexco.moduloProyectos.controller;
+import com.carboexco.moduloProyectos.entity.Personal;
+import com.carboexco.moduloProyectos.repository.PersonalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/personals")
+public class PersonalController {
+
+    @Autowired
+    PersonalRepository personalRepository;
+
+    @GetMapping
+    public List<Personal> getPersonalAll() {
+        return personalRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Personal getPersonalbyId(@PathVariable int id) {
+
+        Optional<Personal> personal = personalRepository.findById(id);
+
+        if (personal.isPresent()) {
+            return personal.get();
+        }
+
+        return null;
+    }
+
+    @PostMapping
+    public Personal postPersonal(@RequestBody Personal personal) {
+        personalRepository.save(personal);
+        return personal;
+    }
+
+    @PutMapping("/{id}")
+    public Personal putPersonalbyId(@PathVariable int id, @RequestBody Personal personal) {
+
+        Optional<Personal> personalCurrent = personalRepository.findById(id);
+
+        if (personalCurrent.isPresent()) {
+            Personal personalReturn = personalCurrent.get();
+
+            personalReturn.setIdCargo(personal.getIdCargo());
+
+            personalRepository.save(personalReturn);
+            return personalReturn;
+        }
+
+        return null;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public Personal deletePersonalbyId(@PathVariable int id) {
+
+        Optional<Personal> personal = personalRepository.findById(id);
+
+        if (personal.isPresent()) {
+            Personal personalReturn = personal.get();
+            personalRepository.deleteById(id);
+            return personalReturn;
+        }
+
+        return null;
+    }
+}
