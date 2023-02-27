56ef134ddd8fe2dba700dd8018ac31a6a35fb19d
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-02-27 11:32:26 -0500

    estructura basica

---
+++ src/main/java/com/carboexco/moduloProyectos/ModuloProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.carboexco.moduloProyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carboexco/moduloProyectos/ModuloProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carboexco.moduloProyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
