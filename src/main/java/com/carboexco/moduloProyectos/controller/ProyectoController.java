6a54054bfb78741a9421e91e7478da970f741d71
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-02-28 08:27:59 -0500

    controlador de proyecto
    controlador basico de proyecto

---
+++ src/main/java/com/carboexco/moduloProyectos/controller/ProyectoController.java
@@ -0,0 +1,79 @@
+package com.carboexco.moduloProyectos.controller;
+import com.carboexco.moduloProyectos.entity.Proyecto;
+import com.carboexco.moduloProyectos.repository.ProyectoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/proyectos")
+public class ProyectoController {
+
+    @Autowired
+    ProyectoRepository proyectoRepository;
+
+    @GetMapping
+    public List<Proyecto> getProyectoAll() {
+        return proyectoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Proyecto getProyectobyId(@PathVariable int id) {
+
+        Optional<Proyecto> proyecto = proyectoRepository.findById(id);
+
+        if (proyecto.isPresent()) {
+            return proyecto.get();
+        }
+
+        return null;
+    }
+
+    @PostMapping
+    public Proyecto postProyecto(@RequestBody Proyecto proyecto) {
+        proyectoRepository.save(proyecto);
+        return proyecto;
+    }
+
+    @PutMapping("/{id}")
+    public Proyecto putProyectobyId(@PathVariable int id, @RequestBody Proyecto proyecto) {
+
+        Optional<Proyecto> proyectoCurrent = proyectoRepository.findById(id);
+
+        if (proyectoCurrent.isPresent()) {
+            Proyecto proyectoReturn = proyectoCurrent.get();
+
+            proyectoReturn.setDescripcionProyecto(proyecto.getDescripcionProyecto());
+            proyectoReturn.setNombreProyecto(proyecto.getNombreProyecto());
+            proyectoReturn.setJustificacion(proyecto.getJustificacion());
+            proyectoReturn.setUbicacion(proyecto.getUbicacion());
+            proyectoReturn.setIdPrioridad(proyecto.getIdPrioridad());
+            proyectoReturn.setIdTipoproyecto(proyecto.getIdTipoproyecto());
+            proyectoReturn.setObjetivoGeneral(proyecto.getObjetivoGeneral());
+            proyectoReturn.setObjetivoEspecifico(proyecto.getObjetivoEspecifico());
+            proyectoRepository.save(proyectoReturn);
+            return proyectoReturn;
+        }
+
+        return null;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public Proyecto deleteProyectobyId(@PathVariable int id) {
+
+        Optional<Proyecto> proyecto = proyectoRepository.findById(id);
+
+        if (proyecto.isPresent()) {
+            Proyecto proyectoReturn = proyecto.get();
+            proyectoRepository.deleteById(id);
+            return proyectoReturn;
+        }
+
+        return null;
+    }
+}
