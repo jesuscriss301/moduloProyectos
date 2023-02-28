85d420924d43a91dcb14566fa2ffb29968c4bba5
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-02-28 09:56:17 -0500

    Update TareaRepository.java
    corrección en id del repository de tarea

--- src/main/java/com/carboexco/moduloProyectos/repository/TareaRepository.java
+++ src/main/java/com/carboexco/moduloProyectos/repository/TareaRepository.java
@@ -1,6 +1,7 @@
 package com.carboexco.moduloProyectos.repository;
 
+import com.carboexco.moduloProyectos.entity.Tarea;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TareaRepository extends JpaRepository<Tarea, TareaId> {
+public interface TareaRepository extends JpaRepository<Tarea, Integer> {
 }
